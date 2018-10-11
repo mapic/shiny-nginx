8d28299a02c08cce71c363b021a15642f3b51072
knutole <knutole@mapic.io>
2018-10-11 22:40:14 +0000

    verify build

--- build.sh
+++ build.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+
+# build
 docker build -t mapic/shiny-nginx .
+
+# verify
+if [ "$?" -gt "0" ]; then
+    echo "Build errored."
+    exit 1
+fi
+
+# push
 docker push mapic/shiny-nginx:latest
